94393c6df8eaf1c4cf889e2c6f10dc9e340737f6
joker69216 <hamaraad883@gmail.com>
2023-01-17 10:37:50 +0300

    add likes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_17_072528) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_17_072559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_17_072528) do
     t.integer "AuthorId"
     t.integer "PostId"
     t.string "Text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "AuthorId"
+    t.integer "PostId"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

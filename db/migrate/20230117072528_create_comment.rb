7aceb7d03fb03447ea0cca3a07d908ef53f00f0b
joker69216 <hamaraad883@gmail.com>
2023-01-17 10:37:05 +0300

    add comments table

---
+++ db/migrate/20230117072528_create_comment.rb
@@ -0,0 +1,10 @@
+class CreateComment < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.integer :AuthorId
+      t.integer :PostId
+      t.string :Text
+      t.timestamps
+    end
+  end
+end

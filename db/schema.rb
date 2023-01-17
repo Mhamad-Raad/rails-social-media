473e14360c4d38bbccada31b0decb8faa99df13c
joker69216 <hamaraad883@gmail.com>
2023-01-17 10:45:15 +0300

    add foreign key and indexes to user and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_17_072613) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_17_073410) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_17_072613) do
     t.integer "LikesCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +48,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_17_072613) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end

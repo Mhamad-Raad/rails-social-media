995b50b2098ba61ad94480995865b0b08a648b91
joker69216 <hamaraad883@gmail.com>
2023-01-18 12:47:29 +0300

    add validations comment and like

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,8 @@ class Like < ApplicationRecord
   belongs_to :post, foreign_key: true, optional: true
   belongs_to :author, class_name: 'User', foreign_key: true, optional: true
 
-  # validate user
+  validate :user_id, numericality: { greater_than_or_equal_to: 0 }
+  validate :post_id, numericality: { greater_than_or_equal_to: 0 } 
 
   after_save :update_post_like_count
 

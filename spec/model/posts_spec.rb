5153359dfe8c6898ad6f952f01be08a4328f04ae
joker69216 <hamaraad883@gmail.com>
2023-01-18 11:21:54 +0300

    add tests to post validations

---
+++ spec/model/posts_spec.rb
@@ -0,0 +1,34 @@
+
+require 'rails_helper'
+require 'spec_helper'
+require 'post'
+
+describe 'Post' do
+  describe "validations" do
+    it "validates title length" do
+      my_model = Post.new(title: "", likes_count: 1, comments_count: 1)
+
+      expect(my_model.valid?).to be_falsey
+      my_model.title = "a"*251
+      expect(my_model.valid?).to be_falsey
+      my_model.title = "a"*240
+      expect(my_model.valid?).to be_truthy
+    end
+
+    it "validates presence of title" do
+      my_model = Post.new(title: "", likes_count: 1, comments_count: 1)
+      expect(my_model.valid?).to be_falsey
+    end
+
+    it "validates comments_count numericality" do
+      my_model = Post.new(title: "hala", likes_count: 1, comments_count: 1)
+      
+      expect(my_model.valid?).to be_truthy
+    end
+
+    it "validates likes_count numericality" do
+      my_model = Post.new(title: "yoooo", likes_count: -1, comments_count: 1)
+      expect(my_model.valid?).to be_falsey
+    end
+  end
+end

d09358b37be88f6ca907524cc820eec8a338d467
joker69216 <hamaraad883@gmail.com>
2023-01-18 12:56:48 +0300

    add test to the commetn validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :post, foreign_key: true, optional: true
   belongs_to :author, class_name: 'User', foreign_key: true, optional: true
 
-  validate :user_id, numericality: { greater_than_or_equal_to: 0 }
-  validate :post_id, numericality: { greater_than_or_equal_to: 0 }
+  validates :user_id, numericality: { greater_than_or_equal_to: 0 }
+  validates :post_id, numericality: { greater_than_or_equal_to: 0 }
 
   after_save :update_post_comment_count
 

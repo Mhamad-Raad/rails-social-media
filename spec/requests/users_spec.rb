a2793c0addf6d1d1390cb15955eaf9a5b789bb6b
joker69216 <hamaraad883@gmail.com>
2023-01-24 22:47:41 +0300

    add tests to the users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  context 'testing GET /users' do
+    it 'should return http success' do
+      get '/users'
+      expect(response).to have_http_status(:success)
+    end
+    it 'should return response body that includes correct placeholder text' do
+      get '/'
+      expect(response.body).to include 'Empty'
+    end
+    it 'should render users index page' do
+      get '/'
+      expect(response).to render_template('index')
+    end
+    context 'testing GET /users' do
+      it 'should render detail page successfuly' do
+        get '/users/1'
+        expect(response).to have_http_status(:success)
+      end
+      it 'should render details'do
+        get '/users/1'
+        expect(response).to render_template('detail')
+      end
+    end
   end
 end

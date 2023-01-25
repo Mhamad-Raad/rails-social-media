61cea395ce7c63e1ff4442b478ed3f11ae07fa75
joker69216 <hamaraad883@gmail.com>
2023-01-26 02:21:34 +0300

    add posts view page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @comments = Comment.all
+  end
 
   def show; end
 end

958d35c9713e95c696552442479a7da4f172f61a
joker69216 <hamaraad883@gmail.com>
2023-02-01 21:14:44 +0300

    add tests for the user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def detail
-    @user = User.find(params[:user_id])
+  def show
+    @user = User.find(params[:id])
   end
 end

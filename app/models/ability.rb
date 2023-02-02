6cf53a276a5a9eb74ba221480ec784917a8a4b4c
joker69216 <hamaraad883@gmail.com>
2023-02-02 21:55:11 +0300

    setup cancan

---
+++ app/models/ability.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :read, :all
+    return unless user.present? 
+    can :read, :all
+    can :manage, User, id: user.id
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
+
+    return unless user.role == 'admin' # admin permissions
+
+    can :manage, :all
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'posts_id'
-  has_many :comments, foreign_key: 'posts_id'
+  has_many :likes, foreign_key: 'posts_id', dependent: :destroy
+  has_many :comments, foreign_key: 'posts_id', dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }

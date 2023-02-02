21870d5b0294fb0a3d5a09955bfa3d8524251f22
joker69216 <hamaraad883@gmail.com>
2023-02-02 09:40:36 +0300

    set up the project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
+
+  has_one_attached :photo
+
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'

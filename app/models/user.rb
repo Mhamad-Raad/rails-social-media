3ec8fe484812af74848e639f1900900252e87d4f
joker69216 <hamaraad883@gmail.com>
2023-02-04 18:27:28 +0300

    fix the required changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_one_attached :photo
 
   validates :name, presence: true
-  # validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def last_three_posts_for_user
     posts.limit(3)
